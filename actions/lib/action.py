0ab13c05c4862a70bef1102fcf300187797e417a
John Anderson <lampwins@gmail.com>
2017-05-09 14:11:37 -0400

    fix wrong cofig key name in base action

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -19,7 +19,7 @@ class NetboxBaseAction(Action):
         """Make a get request to the API URI passed in
         """
         
-        if self.config['use_ssl']:
+        if self.config['use_https']:
             url = 'https://'
         else:
             url = 'http://'
