7cb5f9c87d53190b7085a03f7c9cedf6fc3e41c3
John Anderson <lampwins@gmail.com>
2017-05-22 21:14:04 -0400

    test generic get action

--- actions/dcim_get_devices.py
+++ actions/dcim_get_devices.py
@@ -3,9 +3,9 @@ from lib.action import NetboxBaseAction
 
 
 class NetboxDCIMGetDevice(NetboxBaseAction):
-    """Get a device by serial number"""
+    """Get device(s)"""
 
     def run(self, **kwargs):
-        """Get a device by serial number"""
+        """Get device(s)"""
         endpoint_uri = '/api/dcim/devices/'
         return self.get(endpoint_uri, **kwargs)
